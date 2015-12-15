61d9be8dc83ffeb6edb487ba786b22359387f3c7
Alexandre Konioukhov <alexan1@gmail.com>
2015-12-15 15:51:31 -0500

    initial

---
+++ Student/Grade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public enum Grade
+    {
+        F = 0,
+        D = 1,
+        C = 2,
+        B = 3,
+        A = 4
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class Student
+    {
+        private int sumOfLetterGrades;
+        private int totalClassesTaken;
+
+        public Student(int id)
+        {
+            this.Id = id;
+        }
+
+        public int Id { get; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double GradePointAverage =>
+            (totalClassesTaken != 0) ? (double)sumOfLetterGrades / (double)totalClassesTaken : 0;
+
+        public void RecordClassGrade(Grade letterGrade)
+        {
+            this.sumOfLetterGrades += (int)letterGrade;
+            this.totalClassesTaken++;
+        }
+    }
+}
